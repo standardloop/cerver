8d607f0f09c48d3af065085ac7ce36f60b374043
jbrard <Joshuaeli1234@gmail.com>
2024-02-10 23:38:01 -0700

    cleanup

--- main.c
+++ main.c
@@ -1,17 +1,8 @@
-// Server side C program to demonstrate HTTP Server programming
-#include <stdio.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <string.h>
 #include "./lib/cerver/cerver.h"
 
 int main(int argc, char const *argv[])
 {
-
     int port = 8080;
-
     int status = Cerver(port);
     return (status);
 }
