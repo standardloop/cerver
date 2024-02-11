2b559042be30edbae81f2fdfbcd8829e8595c160
jbrard <Joshuaeli1234@gmail.com>
2024-02-10 23:35:28 -0700

    early but start

---
+++ lib/cerver/cerver.c
@@ -0,0 +1,62 @@
+#include "cerver.h"
+
+#include <stdio.h>      // perror
+#include <sys/socket.h> // socket() bind() listen() accept() sockaddr
+#include <stdlib.h>     // EXIT_FAILURE
+#include <string.h>     // strlen()
+#include <netinet/in.h> // sockaddr_in
+#include <unistd.h>     // write() close()
+
+int Cerver(int port)
+{
+    int server_fd;
+    int new_socket;
+    long valread;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    char *hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("In socket");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+
+    memset(address.sin_zero, '\0', sizeof address.sin_zero);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("In bind");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 10) < 0)
+    {
+        perror("In listen");
+        exit(EXIT_FAILURE);
+    }
+    while (1)
+    {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        {
+            perror("In accept");
+            exit(EXIT_FAILURE);
+        }
+
+        char buffer[30000] = {0};
+        valread = read(new_socket, buffer, 30000);
+        printf("%s\n", buffer);
+        write(new_socket, hello, strlen(hello));
+        close(new_socket);
+    }
+    return EXIT_SUCCESS;
+}
+
+// void Use(char* path, void*() promise) {
+
+// }
